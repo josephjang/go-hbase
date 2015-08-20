facf7c4b086d6e745862da889a36875ebd50e838
Jeongho Jang <jeongho.jang@linecorp.com>
2015-08-20 22:13:39 +0900

    add timestamp for Put

--- put.go
+++ put.go
@@ -11,6 +11,7 @@ type Put struct {
 	key        []byte
 	families   [][]byte
 	qualifiers [][][]byte
+	timestamps [][]uint64
 	values     [][][]byte
 }
 
@@ -19,27 +20,30 @@ func CreateNewPut(key []byte) *Put {
 		key:        key,
 		families:   make([][]byte, 0),
 		qualifiers: make([][][]byte, 0),
+		timestamps: make([][]uint64, 0),
 		values:     make([][][]byte, 0),
 	}
 }
 
-func (this *Put) AddValue(family, column, value []byte) {
+func (this *Put) AddValue(family, column []byte, timestamp uint64, value []byte) {
 	pos := this.posOfFamily(family)
 
 	if pos == -1 {
 		this.families = append(this.families, family)
 		this.qualifiers = append(this.qualifiers, make([][]byte, 0))
+		this.timestamps = append(this.timestamps, make([]uint64, 0))
 		this.values = append(this.values, make([][]byte, 0))
 
 		pos = this.posOfFamily(family)
 	}
 
 	this.qualifiers[pos] = append(this.qualifiers[pos], column)
+	this.timestamps[pos] = append(this.timestamps[pos], timestamp)
 	this.values[pos] = append(this.values[pos], value)
 }
 
-func (this *Put) AddStringValue(family, column, value string) {
-	this.AddValue([]byte(family), []byte(column), []byte(value))
+func (this *Put) AddStringValue(family, column string, timestamp uint64, value string) {
+	this.AddValue([]byte(family), []byte(column), timestamp, []byte(value))
 }
 
 func (this *Put) posOfFamily(family []byte) int {
@@ -65,6 +69,7 @@ func (this *Put) toProto() pb.Message {
 		for j, _ := range this.qualifiers[i] {
 			cv.QualifierValue = append(cv.QualifierValue, &proto.MutationProto_ColumnValue_QualifierValue{
 				Qualifier: this.qualifiers[i][j],
+				Timestamp: &this.timestamps[i][j],
 				Value:     this.values[i][j],
 			})
 		}
